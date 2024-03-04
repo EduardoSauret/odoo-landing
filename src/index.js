7ba6e08225649e87fbef6b498009824acdca14fd
unknown <eduardosauret@gmail.com>
2024-03-04 10:03:00 -0500

    footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './styles/styles.scss';
 import './index.html';
 // import '@fortawesome/fontawesome-free/js/all.js';
+
+// Get all the elements with the class 'card'
+// var cards = document.querySelectorAll('.clickable');
+
+// // Loop through each card
+// for (var i = 0; i < cards.length; i++) {
+//   // Add a click event listener to each card
+//   cards[i].addEventListener('click', function() {
+//     var topValue = parseInt(this.style.top, 10);
+
+//     // Move the card up by 10 pixels
+//     this.style.top = (topValue - 10) + 'px';
+
+//     console.log('card clicked');
+//   });
+// }
--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,19 @@ import '../styles/styles.scss';
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
+// Get all the elements with the class 'card'
+var cards = document.querySelectorAll('.clickable');
 
-addEventListener('DOMContentLoaded', function(){
-    
-});
+// Loop through each card
+for (var i = 0; i < cards.length; i++) {
+  // Add a click event listener to each card
+  cards[i].addEventListener('click', function() {
+    var topValue = parseInt(this.style.top, 10);
+
+    // Move the card up by 10 pixels
+    this.style.position = 'relative';
+    this.style.top = (topValue - 10) + '10px';
+
+
+  });
+}
